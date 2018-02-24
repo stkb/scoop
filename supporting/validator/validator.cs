d8b06a59c06589a9f8179a7879bfaf36cb8dbf24
Steve Baker <_steve_@outlook.com>
2018-02-24 20:09:44 +0100

    Validator: allow passing in multiple manifests

--- supporting/validator/validator.cs
+++ supporting/validator/validator.cs
@@ -1,5 +1,7 @@
 using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 
 namespace Scoop
 {
@@ -7,31 +9,41 @@ namespace Scoop
     {
         public static int Main(string[] args)
         {
-            bool ci = (args.Length == 3 && args[2] == "-ci");
-            bool valid = false;
+            string schema = null;
+            string[] otherArgs = args.Where(x => x != "-ci").ToArray();
+            bool ci = otherArgs.Length < args.Length;
+            string[] manifests = new string[0];
 
-            if (args.Length < 2)
+            if (otherArgs.Length >= 2) 
+            {
+                schema = otherArgs[0];
+                manifests = otherArgs.Skip(1).ToArray();
+            }
+
+            if (manifests.Length == 0)
             {
                 Console.WriteLine("Usage: validator.exe schema.json manifest.json");
                 return 1;
             }
 
-            Scoop.Validator validator = new Scoop.Validator(args[0], ci);
-            valid = validator.Validate(args[1]);
+            Scoop.Validator validator = new Scoop.Validator(schema, ci);
 
-            if (valid)
+            bool allValid = true;
+
+            foreach(string manifest in manifests) 
             {
-                Console.WriteLine("Yay! {0} validates against the schema!", Path.GetFileName(args[1]));
-            }
-            else
-            {
-                foreach (var error in validator.Errors)
+                if (validator.Validate(manifest)) 
                 {
-                    Console.WriteLine(error);
+                    Console.WriteLine("Yay! {0} validates against the schema!", Path.GetFileName(manifest));
+                }
+                else 
+                {
+                    validator.Errors.ToList().ForEach(Console.WriteLine);
+                    allValid = false;
                 }
             }
 
-            return valid ? 0 : 1;
+            return allValid ? 0 : 1;
         }
     }
 }
